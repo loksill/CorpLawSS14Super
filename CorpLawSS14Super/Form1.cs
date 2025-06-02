613ce6af3aae9fcf311c860e61fece5e313fc0ce
Loksik <loksasaloktionov@yandex.ru>
2025-06-02 21:42:34 +0300

    Поддержка высших мер

--- CorpLawSS14Super/Form1.cs
+++ CorpLawSS14Super/Form1.cs
@@ -16,6 +16,7 @@ namespace CorpLawSS14Super
 
         private int parameterSum = 0; // Текущая сумма
         private int parameterToAdd = 0; // Параметр, который будет добавляться
+        private string parameterOther = "Высшие меры:";
 
         public Form1()
         {
@@ -35,7 +36,7 @@ namespace CorpLawSS14Super
         //    { "Организатор", 10},
         //    //{ "Неосторожность", -5}
 
-        private void button1_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e) // XX1 | 5 минут
         {
             // Берём число, которое добавляем
             parameterToAdd += 5;
@@ -44,10 +45,73 @@ namespace CorpLawSS14Super
             parameterSum += parameterToAdd;
 
             // Обновляем label2 с новой суммой
-            label2.Text = $"Текущая сумма: {parameterSum}";
+            label2.Text = $"Время: {parameterSum}";
 
             // Обнуляем значение
             parameterToAdd = 0;
+        }
+
+        private void button2_Click(object sender, EventArgs e) // XX2 | 10 минут
+        {
+            // Берём число, которое добавляем
+            parameterToAdd += 10;
+
+            // Добавляем параметр к сумме
+            parameterSum += parameterToAdd;
+
+            // Обновляем label2 с новой суммой
+            label2.Text = $"Время: {parameterSum}";
+
+            // Обнуляем значение
+            parameterToAdd = 0;
+        }
+
+        private void button3_Click(object sender, EventArgs e) // XX3 | 15 минут
+        {
+            // Берём число, которое добавляем
+            parameterToAdd += 15;
+
+            // Добавляем параметр к сумме
+            parameterSum += parameterToAdd;
+
+            // Обновляем label2 с новой суммой
+            label2.Text = $"Время: {parameterSum}";
+
+            // Обнуляем значение
+            parameterToAdd = 0;
+        }
+
+        private void button4_Click(object sender, EventArgs e) // XX4 | 25 минут
+        {
+            // Берём число, которое добавляем
+            parameterToAdd += 25;
+
+            // Добавляем параметр к сумме
+            parameterSum += parameterToAdd;
+
+            // Обновляем label2 с новой суммой
+            label2.Text = $"Время: {parameterSum}";
+
+            // Обнуляем значение
+            parameterToAdd = 0;
+        }
+
+        private void button5_Click(object sender, EventArgs e) // XX5 | Пожизненное
+        {
+            // Добавляем вердикт
+            parameterOther += " пожизненное,";
+
+            // Обновляем label6
+            label6.Text = $"{parameterOther}";
+        }
+
+        private void button6_Click(object sender, EventArgs e) // XX6 | Смертная казнь
+        {
+            // Добавляем вердикт
+            parameterOther += " cмертная казнь,";
+
+            // Обновляем label6
+            label6.Text = $"{parameterOther}";
         }
     }
 }
