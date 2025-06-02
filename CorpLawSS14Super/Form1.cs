a34877e6e369dac31c0a39065cd07212f53e07f8
Loksik <loksasaloktionov@yandex.ru>
2025-06-02 20:47:03 +0300

    Добавьте файлы проекта.

---
+++ CorpLawSS14Super/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CorpLawSS14Super
+{
+    public partial class Form1 : Form
+
+    {
+
+        private int parameterSum = 0; // Текущая сумма
+        private int parameterToAdd = 0; // Параметр, который будет добавляться
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Добавляем сумму
+            parameterToAdd += 5;
+
+            // Добавляем параметр к сумме
+            parameterSum += parameterToAdd;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Обновляем label2 с новой суммой
+            label2.Text = $"{parameterSum}";
+        }
+    }
+}
