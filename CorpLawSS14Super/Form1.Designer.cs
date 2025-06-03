7376c69c1f0ddc9115a69689b14687588674ed27
Loksik <loksasaloktionov@yandex.ru>
2025-06-03 08:40:02 +0300

    Модификаторы

--- CorpLawSS14Super/Form1.Designer.cs
+++ CorpLawSS14Super/Form1.Designer.cs
@@ -42,6 +42,24 @@ namespace CorpLawSS14Super
             this.label1 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button16 = new System.Windows.Forms.Button();
+            this.button17 = new System.Windows.Forms.Button();
+            this.button18 = new System.Windows.Forms.Button();
+            this.button19 = new System.Windows.Forms.Button();
+            this.button20 = new System.Windows.Forms.Button();
+            this.button21 = new System.Windows.Forms.Button();
+            this.button22 = new System.Windows.Forms.Button();
+            this.button23 = new System.Windows.Forms.Button();
+            this.button24 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -116,9 +134,9 @@ namespace CorpLawSS14Super
             this.label5.AutoSize = true;
             this.label5.Location = new System.Drawing.Point(246, 36);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(50, 13);
+            this.label5.Size = new System.Drawing.Size(51, 13);
             this.label5.TabIndex = 10;
-            this.label5.Text = "Особые:";
+            this.label5.Text = "Высшие:";
             // 
             // button5
             // 
@@ -166,11 +184,208 @@ namespace CorpLawSS14Super
             this.button7.UseVisualStyleBackColor = true;
             this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(48, 55);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(25, 23);
+            this.button8.TabIndex = 15;
+            this.button8.Text = "<";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(48, 84);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(25, 23);
+            this.button9.TabIndex = 16;
+            this.button9.Text = "<";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(48, 113);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(25, 23);
+            this.button10.TabIndex = 17;
+            this.button10.Text = "<";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(48, 142);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(25, 23);
+            this.button11.TabIndex = 18;
+            this.button11.Text = "<";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(203, 55);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(25, 23);
+            this.button12.TabIndex = 19;
+            this.button12.Text = "<";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(203, 84);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(25, 23);
+            this.button13.TabIndex = 20;
+            this.button13.Text = "<";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(403, 36);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(88, 13);
+            this.label7.TabIndex = 21;
+            this.label7.Text = "Модификаторы:";
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(599, 55);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(162, 23);
+            this.button14.TabIndex = 22;
+            this.button14.Text = "Неосторожность";
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.button14_Click);
+            // 
+            // button16
+            // 
+            this.button16.Location = new System.Drawing.Point(599, 84);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(162, 23);
+            this.button16.TabIndex = 24;
+            this.button16.Text = "Отсутствие вины";
+            this.button16.UseVisualStyleBackColor = true;
+            this.button16.Click += new System.EventHandler(this.button16_Click);
+            // 
+            // button17
+            // 
+            this.button17.Location = new System.Drawing.Point(406, 55);
+            this.button17.Name = "button17";
+            this.button17.Size = new System.Drawing.Size(162, 23);
+            this.button17.TabIndex = 25;
+            this.button17.Text = "Организатор";
+            this.button17.UseVisualStyleBackColor = true;
+            this.button17.Click += new System.EventHandler(this.button17_Click);
+            // 
+            // button18
+            // 
+            this.button18.Location = new System.Drawing.Point(599, 113);
+            this.button18.Name = "button18";
+            this.button18.Size = new System.Drawing.Size(162, 23);
+            this.button18.TabIndex = 26;
+            this.button18.Text = "Крайняя необходимость";
+            this.button18.UseVisualStyleBackColor = true;
+            this.button18.Click += new System.EventHandler(this.button18_Click);
+            // 
+            // button19
+            // 
+            this.button19.Location = new System.Drawing.Point(599, 142);
+            this.button19.Name = "button19";
+            this.button19.Size = new System.Drawing.Size(162, 23);
+            this.button19.TabIndex = 27;
+            this.button19.Text = "Гипноз";
+            this.button19.UseVisualStyleBackColor = true;
+            this.button19.Click += new System.EventHandler(this.button19_Click);
+            // 
+            // button20
+            // 
+            this.button20.Location = new System.Drawing.Point(599, 171);
+            this.button20.Name = "button20";
+            this.button20.Size = new System.Drawing.Size(162, 23);
+            this.button20.TabIndex = 28;
+            this.button20.Text = "Допустимая самооборона";
+            this.button20.UseVisualStyleBackColor = true;
+            this.button20.Click += new System.EventHandler(this.button20_Click);
+            // 
+            // button21
+            // 
+            this.button21.Location = new System.Drawing.Point(406, 84);
+            this.button21.Name = "button21";
+            this.button21.Size = new System.Drawing.Size(162, 23);
+            this.button21.TabIndex = 29;
+            this.button21.Text = "Расизм";
+            this.button21.UseVisualStyleBackColor = true;
+            this.button21.Click += new System.EventHandler(this.button21_Click);
+            // 
+            // button22
+            // 
+            this.button22.Location = new System.Drawing.Point(406, 113);
+            this.button22.Name = "button22";
+            this.button22.Size = new System.Drawing.Size(162, 23);
+            this.button22.TabIndex = 30;
+            this.button22.Text = "Должностное преступление";
+            this.button22.UseVisualStyleBackColor = true;
+            this.button22.Click += new System.EventHandler(this.button22_Click);
+            // 
+            // button23
+            // 
+            this.button23.Location = new System.Drawing.Point(406, 142);
+            this.button23.Name = "button23";
+            this.button23.Size = new System.Drawing.Size(162, 23);
+            this.button23.TabIndex = 31;
+            this.button23.Text = "Против должностного лица";
+            this.button23.UseVisualStyleBackColor = true;
+            this.button23.Click += new System.EventHandler(this.button23_Click);
+            // 
+            // button24
+            // 
+            this.button24.Location = new System.Drawing.Point(599, 201);
+            this.button24.Name = "button24";
+            this.button24.Size = new System.Drawing.Size(162, 23);
+            this.button24.TabIndex = 32;
+            this.button24.Text = "Явка с повинной";
+            this.button24.UseVisualStyleBackColor = true;
+            this.button24.Click += new System.EventHandler(this.button24_Click);
+            // 
+            // button15
+            // 
+            this.button15.Location = new System.Drawing.Point(406, 171);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(162, 53);
+            this.button15.TabIndex = 33;
+            this.button15.Text = "Рецедив (добавьте столько, сколько нужно)";
+            this.button15.UseVisualStyleBackColor = true;
+            this.button15.Click += new System.EventHandler(this.button15_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1058, 596);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button24);
+            this.Controls.Add(this.button23);
+            this.Controls.Add(this.button22);
+            this.Controls.Add(this.button21);
+            this.Controls.Add(this.button20);
+            this.Controls.Add(this.button19);
+            this.Controls.Add(this.button18);
+            this.Controls.Add(this.button17);
+            this.Controls.Add(this.button16);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
             this.Controls.Add(this.button7);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label1);
@@ -206,6 +421,24 @@ namespace CorpLawSS14Super
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button16;
+        private System.Windows.Forms.Button button17;
+        private System.Windows.Forms.Button button18;
+        private System.Windows.Forms.Button button19;
+        private System.Windows.Forms.Button button20;
+        private System.Windows.Forms.Button button21;
+        private System.Windows.Forms.Button button22;
+        private System.Windows.Forms.Button button23;
+        private System.Windows.Forms.Button button24;
+        private System.Windows.Forms.Button button15;
     }
 }
 
--- CorpLawSS14Super/Form1.cs
+++ CorpLawSS14Super/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
 
 namespace CorpLawSS14Super
 {
@@ -16,7 +17,10 @@ namespace CorpLawSS14Super
 
         private int parameterSum = 0; // Текущая сумма
         private int parameterToAdd = 0; // Параметр, который будет добавляться
+        private int parameterBackSum = 0; // Удаление лишнего
+        private int parameterMod = 0; // Модификаторы
         private string parameterOther = "Высшие меры:"; // Высшие меры
+        private string parameterBackOther = ""; // Удаление лишнего в высших мерах
 
         public Form1()
         {
@@ -120,10 +124,277 @@ namespace CorpLawSS14Super
             parameterSum = 0; // Текущая сумма
             parameterToAdd = 0; // Параметр, который будет добавляться
             parameterOther = "Высшие меры:"; // Высшие меры
+            parameterBackSum = 0; // Удаление лишнего
+            parameterBackOther = "Вот такие пироги"; // Удаление лишнего в высших мерах
 
             // Обновляем метки
             label6.Text = $"{parameterOther}";
             label2.Text = $"Время: {parameterSum}";
+        }
+
+        private void button8_Click(object sender, EventArgs e) // XX1 | 5 минут
+        {
+            // Решаем, сколько убираем
+            parameterBackSum += 5; // XX1 | 5 минут
+
+            // Убираем это из срока
+            parameterSum -= parameterBackSum;
+
+            // Обновляем метку
+            label2.Text = $"Время: {parameterSum}";
+
+            // Очищаем убираемое
+            parameterBackSum = 0;
+        }
+
+        private void button9_Click(object sender, EventArgs e) // XX2 | 10 минут
+        {
+            // Решаем, сколько убираем
+            parameterBackSum += 10; // XX2 | 10 минут
+
+            // Убираем это из срока
+            parameterSum -= parameterBackSum;
+
+            // Обновляем метку
+            label2.Text = $"Время: {parameterSum}";
+
+            // Очищаем убираемое
+            parameterBackSum = 0;
+        }
+
+        private void button10_Click(object sender, EventArgs e) // XX3 | 15 минут
+        {
+            // Решаем, сколько убираем
+            parameterBackSum += 15; // XX3 | 15 минут
+
+            // Убираем это из срока
+            parameterSum -= parameterBackSum;
+
+            // Обновляем метку
+            label2.Text = $"Время: {parameterSum}";
+
+            // Очищаем убираемое
+            parameterBackSum = 0;
+        }
+
+        private void button11_Click(object sender, EventArgs e) // XX4 | 25 минут
+        {
+            // Решаем, сколько убираем
+            parameterBackSum += 25; // XX4 | 25 минут
+
+            // Убираем это из срока
+            parameterSum -= parameterBackSum;
+
+            // Обновляем метку
+            label2.Text = $"Время: {parameterSum}";
+
+            // Очищаем убираемое
+            parameterBackSum = 0;
+        }
+
+        private void button12_Click(object sender, EventArgs e) // XX5 | Пожизненное
+        {
+            // Решаем, что убирать
+            parameterBackOther = " пожизненное,";
+
+            // Убираем, что хотим
+            string result = parameterOther.Replace(parameterBackOther, "");
+
+            // Обновляем метку
+            label6.Text = $"{result}";
+
+            // Обновляем переменные
+            parameterBackOther = "";
+            parameterOther = result;
+        }
+
+        private void button13_Click(object sender, EventArgs e) // XX6 | Смертная казнь
+        {
+            // Решаем, что убирать
+            parameterBackOther = " cмертная казнь,";
+
+            // Убираем, что хотим
+            string result = parameterOther.Replace(parameterBackOther, "");
+
+            // Обновляем метку
+            label6.Text = $"{result}";
+
+            // Обновляем переменные
+            parameterBackOther = "";
+            parameterOther = result;
+        }
+
+        private void button17_Click(object sender, EventArgs e) // Организатор | +10 минут
+        {
+            // Выбираем, сколько хотим убрать/добавить
+            parameterMod = 10;
+
+            // Применяем модификатор
+            parameterSum += parameterMod;
+
+            // Обновляем label2
+            label2.Text = $"Время: {parameterSum}";
+
+            // Очищаем переременную
+            parameterMod = 0;
+        }
+
+        private void button21_Click(object sender, EventArgs e) // Расизм | +10 минут
+        {
+            // Выбираем, сколько хотим убрать/добавить
+            parameterMod = 10;
+
+            // Применяем модификатор
+            parameterSum += parameterMod;
+
+            // Обновляем label2
+            label2.Text = $"Время: {parameterSum}";
+
+            // Очищаем переременную
+            parameterMod = 0;
+        }
+
+        private void button22_Click(object sender, EventArgs e) // Должностное преступление | +10 минут
+        {
+            // Выбираем, сколько хотим убрать/добавить
+            parameterMod = 10;
+
+            // Применяем модификатор
+            parameterSum += parameterMod;
+
+            // Обновляем label2
+            label2.Text = $"Время: {parameterSum}";
+
+            // Очищаем переременную
+            parameterMod = 0;
+        }
+
+        private void button23_Click(object sender, EventArgs e) // Против должностного лица | +10 минут
+        {
+            // Выбираем, сколько хотим убрать/добавить
+            parameterMod = 10;
+
+            // Применяем модификатор
+            parameterSum += parameterMod;
+
+            // Обновляем label2
+            label2.Text = $"Время: {parameterSum}";
+
+            // Очищаем переременную
+            parameterMod = 0;
+        }
+
+        private void button15_Click(object sender, EventArgs e) // Рецедив | +5 минут
+        {
+            // Выбираем, сколько хотим убрать/добавить
+            parameterMod = 5;
+
+            // Применяем модификатор
+            parameterSum += parameterMod;
+
+            // Обновляем label2
+            label2.Text = $"Время: {parameterSum}";
+
+            // Очищаем переременную
+            parameterMod = 0;
+        }
+
+        private void button14_Click(object sender, EventArgs e) // Неосторожность | -5 минут
+        {
+            // Выбираем, сколько хотим убрать/добавить
+            parameterMod = -5;
+
+            // Применяем модификатор
+            parameterSum += parameterMod;
+
+            // Обновляем label2
+            label2.Text = $"Время: {parameterSum}";
+
+            // Очищаем переременную
+            parameterMod = 0;
+        }
+
+        private void button16_Click(object sender, EventArgs e) // Отсутствие вины | Сятие обвинений
+        {
+            // Выбираем, сколько хотим убрать/добавить
+            parameterMod = -parameterSum;
+
+            // Применяем модификатор
+            parameterSum += parameterMod;
+            parameterOther = "Высшие меры:"; // Снимаем пожизненные и казни
+
+            // Обновляем метки
+            label2.Text = $"Время: {parameterSum}";
+            label6.Text = $"{parameterOther}";
+
+            // Очищаем переременную
+            parameterMod = 0;
+        }
+
+        private void button18_Click(object sender, EventArgs e) // Крайняя необходимость | Сятие обвинений
+        {
+            // Выбираем, сколько хотим убрать/добавить
+            parameterMod = -parameterSum;
+
+            // Применяем модификатор
+            parameterSum += parameterMod;
+            parameterOther = "Высшие меры:"; // Снимаем пожизненные и казни
+
+            // Обновляем метки
+            label2.Text = $"Время: {parameterSum}";
+            label6.Text = $"{parameterOther}";
+
+            // Очищаем переременную
+            parameterMod = 0;
+        }
+
+        private void button19_Click(object sender, EventArgs e) // Гипноз | Сятие обвинений
+        {
+            // Выбираем, сколько хотим убрать/добавить
+            parameterMod = -parameterSum;
+
+            // Применяем модификатор
+            parameterSum += parameterMod;
+            parameterOther = "Высшие меры:"; // Снимаем пожизненные и казни
+
+            // Обновляем метки
+            label2.Text = $"Время: {parameterSum}";
+            label6.Text = $"{parameterOther}";
+
+            // Очищаем переременную
+            parameterMod = 0;
+        }
+
+        private void button20_Click(object sender, EventArgs e) // Допустимая самооборона | Сятие обвинений
+        {
+            // Выбираем, сколько хотим убрать/добавить
+            parameterMod = -parameterSum;
+
+            // Применяем модификатор
+            parameterSum += parameterMod;
+            parameterOther = "Высшие меры:"; // Снимаем пожизненные и казни
+
+            // Обновляем метки
+            label2.Text = $"Время: {parameterSum}";
+            label6.Text = $"{parameterOther}";
+
+            // Очищаем переременную
+            parameterMod = 0;
+        }
+
+        private void button24_Click(object sender, EventArgs e) // Явка с повинной | -5 минут
+        {
+            // Выбираем, сколько хотим убрать/добавить
+            parameterMod = -5;
+
+            // Применяем модификатор
+            parameterSum += parameterMod;
+
+            // Обновляем label2
+            label2.Text = $"Время: {parameterSum}";
+
+            // Очищаем переременную
+            parameterMod = 0;
         }
     }
 }
